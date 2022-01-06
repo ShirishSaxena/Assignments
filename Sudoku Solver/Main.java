de309c05e51dc3d947dce94b6365d941be882386
Shirish Saxena <6762915+ShirishSaxena@users.noreply.github.com>
2022-01-06 18:34:13 +0530

    Added simple menu with 3 options

--- Sudoku Solver/Main.java
+++ Sudoku Solver/Main.java
@@ -1,15 +1,46 @@
 package com.sudoku;
 
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-/*        Integer[][] board = {
+
+        /*
+        *   Error 1 : Input is not in range of 0-9 (inclusive)
+        * */
+        // Uncomment below lines to generate random sudoku
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("\nWelcome to Sudoku Solver\n1). Hardcoded Sudoku\n2). Input a sudoku\n3). Randomize Sudokus\n4). Exit\n\n");
+        System.out.print("Your choice : ");
+
+        int choice = s.nextInt();
+
+        while(choice <= 0 || choice > 4){
+            System.out.print("Invalid choice : ");
+            choice = s.nextInt();
+        }
+
+        switch(choice){
+            case 1:
+                Choice1();
+                break;
+            case 2:
+                Choice2();
+                break;
+            case 3:
+                System.out.print("\nEnter no of sudoku to generate : ");
+                GenerateRandom generateRandom = new GenerateRandom(s.nextInt());
+                break;
+            default:
+                System.out.println("\nThank you");
+                System.exit(0);
+        }
+    }
+
+    static public void Choice1(){
+        Integer[][] board = {
                 { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
                 { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
                 { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
@@ -22,31 +53,14 @@ public class Main {
         };
 
         SudokuSolver sudo = new SudokuSolver(board);
-        sudo.SudokuSolve();*/
+        System.out.print("\nHardcoded sudoku is : ");
+        sudo.print();
+        System.out.print("\nSolved : ");
+        sudo.SudokuSolve();
+    }
 
-        // Uncomment below lines to generate random sudoku
-
-        int generateRange = 20000;
-        List<Integer[][]> list = new ArrayList<>();
-        for(int i = 0; i < generateRange; i++){
-            GenerateRandomSudoku generateRandomSudoku = new GenerateRandomSudoku();
-            list.add(generateRandomSudoku.GenerateRandom());
-        }
-
-        System.out.println("List size : " + list.size());
-        int invalid = 0;
-        long start = System.currentTimeMillis();
-        for(Integer[][] temp : list){
-
-            SudokuSolver s = new SudokuSolver(temp);
-            //s.print(temp);
-            invalid += s.SudokuSolve();
-
-        }
-
-        long end = System.currentTimeMillis();
-        System.out.println("Time took to execute " + list.size() + " sudoku : " + (double)(end - start)/1000 + " secs");
-        System.out.println("\nInvalid Sudoku's : " + invalid + "\nValid Sudoku's : " + (list.size() - invalid));
-
+    static public void Choice2(){
+        SudokuSolver sudo = new SudokuSolver();
+        sudo.takeInput();
     }
 }
