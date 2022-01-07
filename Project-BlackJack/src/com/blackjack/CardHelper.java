480e3eb653903499a6e8e201a08678a927d4c1d1
Shirish Saxena <6762915+ShirishSaxena@users.noreply.github.com>
2022-01-07 20:03:56 +0530

    Add files via upload

---
+++ Project-BlackJack/src/com/blackjack/CardHelper.java
@@ -0,0 +1,33 @@
+package com.blackjack;
+
+// Helper function for card return...
+public class CardHelper {
+    private int n;
+    private String cardName;
+
+    public CardHelper(int n, String cardName) {
+        this.n = n;
+        this.cardName = cardName;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public String toString() {
+        return "[" + ((this.n == 11) ? "ACE" : this.n) + "] of '" + this.cardName + "'";
+    }
+
+}
---
+++ Project-BlackJack/src/com/blackjack/MainRunner.java
@@ -0,0 +1,22 @@
+package com.blackjack;
+
+import java.util.Scanner;
+
+public class MainRunner {
+    public static void main(String[] args) {
+        // Main assignment runner obj
+        Scanner s = new Scanner(System.in);
+        String replay = "y";
+
+        while (replay.equals("y") || replay.equals("yes")) {
+            new BlackJack();
+
+            System.out.print("\n\nPlay again? (y/n) : ");
+            replay = s.next();
+
+            System.out.println("\n\n");
+        }
+
+        System.out.println("Thanks for playing.");
+    }
+}
